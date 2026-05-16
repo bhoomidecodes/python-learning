a559f09298ea88f43f7461b6c1fd3ab97b0ef3fc
bhoomidecodes <bhoomi.codes@gmail.com>
2026-05-16 16:52:17 +0530

    Create variables.py

---
+++ Day-1-Variables/variables.py
@@ -0,0 +1,43 @@
+# Printing output
+print("bhoomi sharma")
+
+# Single-line comment
+
+'''
+Multi-line comment
+'''
+
+# Indentation => spaces at the start of a line to define a block of code.
+age = 32
+if age > 30:
+    print(age)
+
+# Line continuation using backslash
+total = 1+2+3+4+5+6+7+8 + \
+        4+5+6
+print(total)
+
+# Multiple statements in one line
+x = 5; e = 10; z = x + e
+print(z)
+
+# Variable assignment & type checking
+age1 = 32
+print(type(age1))
+
+name = "bhoomi"
+print(type(name)) #type checking
+
+variable = 10
+print(type(variable))
+
+variable = "irish"        
+print(type(variable)) #type checking
+
+# Taking input
+name = input("Enter your name: ")
+print(name)
+
+# Type conversion
+age = int(input("Enter your age: "))
+print(age)
